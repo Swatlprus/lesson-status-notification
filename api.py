ec5417a8315ce23a6c14d87b2faa1bdcdac7915d
Rinat <swatlprus@gmail.com>
2023-06-13 16:43:11 +0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,67 @@
+import requests
+import time
+import json
+from environs import Env
+
+import telegram
+
+
+def send_message(lessons, telegram_token, chat_id):
+    for lesson in lessons:
+        lesson_title = lesson['lesson_title']
+        error = lesson['is_negative']
+        url = lesson['lesson_url']
+        if error:
+            description = 'К сожалению в работе нашлись ошибки.'
+        else:
+            description = 'Преподавателю все понравилось,\
+                можно приступать к следующему уроку!'
+        bot = telegram.Bot(token=telegram_token)
+        bot.send_message(
+            chat_id=chat_id,
+            text=f'У вас проверили работу «{lesson_title}» \n\n{description}\
+                \n\nСсылка на урок: {url}'
+        )
+
+
+def get_notification(dvmn_token, telegram_token, chat_id, payload={}):
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': dvmn_token}
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=95,
+            params=payload,
+        )
+        response.raise_for_status()
+        json_response = json.loads(response.text)
+        if json_response['status'] == 'timeout':
+            payload = {'timestamp': json_response['timestamp_to_request']}
+            get_notification(dvmn_token, payload)
+        elif json_response['status'] == 'found':
+            lessons = json_response['new_attempts']
+            send_message(lessons, telegram_token, chat_id)
+    except requests.exceptions.HTTPError as err:
+        print('Ooops. HTTP Error occurred')
+        print('Response is: {content}'.format(content=err.response.content))
+    except requests.exceptions.ReadTimeout as err:
+        print('Ooops. ReadTimeout Error occurred')
+        print('Response is: {content}'.format(content=err))
+        print('Wait... I will try to send the request again')
+        time.sleep(10)
+    except requests.exceptions.ConnectionError as err:
+        print('Ooops. ConnectionError occurred')
+        print('Response is: {content}'.format(content=err))
+        print('Wait... I will try to send the request again')
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    dvmn_token = env('DVMN_TOKEN')
+    telegram_token = env('TELEGRAM_TOKEN')
+    chat_id = env('CHAT_ID')
+    while True:
+        get_notification(dvmn_token, telegram_token, chat_id)
